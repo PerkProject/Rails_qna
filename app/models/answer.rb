651299393857e99319d1cc69e06f19fddd84c79d
perkir <kspersionov@gmail.com>
2016-12-02 02:31:55 +0300

    add files to answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,18 +2,18 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
-  has_many :attachments, as: :attachmentable
+  has_many :attachments, as: :attachable
 
   validates :body, :user_id, presence: true
 
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank
 
   default_scope { order(best: :desc) }
 
   def mark_as_best
     Answer.transaction do
       question.answers.update_all(best: false)
-      raise ActiveRecord::Rollback if question.answers.where(best: false).exists?(true) # .first.nil?
+      raise ActiveRecord::Rollback if question.answers.where(best: false).exists?(true)
       update(best: true)
     end
   end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
+  it { should have_many :attachments }
   it { should have_db_index :question_id }
   it { should validate_presence_of :body }
   it { should validate_presence_of(:user_id)}
+  it { should accept_nested_attributes_for :attachments }
 
   let(:user){ create(:user) }
   let(:question) {create(:question, user: user)}
