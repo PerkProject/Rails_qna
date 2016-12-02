97b378a8e09aba335342db5a9e380ddeebe752b9
perkir <kspersionov@gmail.com>
2016-12-02 03:29:38 +0300

    add spec for association

--- spec/acceptance/answer/add_files_to_answer_spec.rb
+++ spec/acceptance/answer/add_files_to_answer_spec.rb
@@ -12,15 +12,29 @@ feature 'Add files to answer', %q{
   background do
     sign_in(user)
     visit question_path(question)
+    fill_in 'Your answer', with: 'My answer'
   end
 
   scenario 'User adds file to answer', js: true do
-    fill_in 'Your answer', with: 'My answer'
+
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Create'
 
     within '.answers' do
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
+  end
+
+  scenario 'User can attach two files to answer', js: true do
+
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add file'
+    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    click_on 'Create'
+
+    within '.answers' do
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
     end
   end
 end
--- spec/acceptance/question/add_files_to_question_spec.rb
+++ spec/acceptance/question/add_files_to_question_spec.rb
@@ -11,14 +11,25 @@ feature 'Add files to question', %q{
   background do
     sign_in(user)
     visit new_question_path
-  end
-
-  scenario 'User adds file when asks question' do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
+  end
+
+  scenario 'User adds file when asks question'  do
+
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Create'
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+
+  scenario 'User can attach two files to question', js:true do
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add file'
+    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    click_on 'Create'
+
+    expect(page).to have_content 'spec_helper.rb'
+    expect(page).to have_content 'rails_helper.rb'
   end
 end
