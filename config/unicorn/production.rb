db9c66aa16ed6448681d64084148145cd927afea
perkir <kspersionov@gmail.com>
2017-02-08 04:08:56 +0300

    unicorn to shared

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,7 +3,7 @@ app_path = "/home/deployer/qna"
 working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 # listen
-listen "/tmp/unicorn.qna.sock", backlog: 64
+listen "/home/deployer/qna/shared/tmp/sockets/unicorn.qna.sock", backlog: 64
 # logging
 stderr_path "log/unicorn.stderr.log"
 stdout_path "log/unicorn.stdout.log"
